2d1dde6f530044f25bde51818edde17a6489a84b
Sunitha Malipeddi <sunithamalipeddi@Sunithas-Air.home>
2018-07-24 07:18:01 -0400

    review fixes

--- service-worker.js
+++ service-worker.js
@@ -31,6 +31,10 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
